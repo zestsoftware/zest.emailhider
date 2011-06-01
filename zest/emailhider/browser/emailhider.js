54a936ef7a98ec24dcb91efd6e630d8f908aba5e
Vincent Pretre <v.pretre@zestsoftware.nl>
2011-06-01 14:17:11 +0000

    Updated ajax call
    svn path=/zest.emailhider/trunk/; revision=240270

--- zest/emailhider/browser/emailhider.js
+++ zest/emailhider/browser/emailhider.js
@@ -1,6 +1,9 @@
 jq(document).ready(function() {
+    var reveal_url = 'jq_reveal_email'
+    if (typeof(portal_url) != 'undefined') {
+	reveal_url = portal_url + '/' + reveal_url;
+    }
     jq('a.hidden-email').each(function() {
-        jq.pyproxy_call('jq_reveal_email',
-                        {'uid': jq(this).attr('rel')});
+        jq.pyproxy_call(reveal_url, {'uid': jq(this).attr('rel')});
     });
 });
